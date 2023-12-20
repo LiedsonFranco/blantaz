ebbd1a356a315bd267bc7829d51eebfd51c69601
Liedson Franco <liedsonfranco594@gmail.com>
2023-12-20 10:49:02 +0000

    members page and members files added

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,78 @@
 const PICTURE = {
-    'desktop': ['IMG_1.jpg','IMG_2.jpg'],
-    'mobile': ['IMG_1.jpg','IMG_2.jpg','IMG_3.jpg'],
+    'desktop': ['IMG_1.jpg', 'IMG_2.jpg'],
+    'mobile': ['IMG_1.jpg', 'IMG_2.jpg', 'IMG_3.jpg'],
+    'menbers': [
+        {
+            'img': '1.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '2.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '3.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '4.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '5.jpg',
+            'name': 'Black',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '6.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        },
+        {
+            'img': '7.jpg',
+            'name': 'Black e ..',
+            'description': 'Qualquer coisa'
+        }
+    ]
 }
 let current_picture = 0
 let current_device = 'desktop'
 
-function play_video(){
+function play_video() {
     let slideshow = document.querySelector('.slideshow')
-    slideshow.innerHTML = '<iframe width="100%" height="480" src="https://www.youtube.com/embed/HHzIzkPhhaE" title="Rap do Law (One Piece) - CIRURGIÃO DA MORTE | NERD HITS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    slideshow.innerHTML = '<iframe width="100%" height="480" src="https://www.youtube.com/embed/hh0Ug2obslE" title="Déjà Vu - BLANTZ [Prod: Djasmim No Beatz]( Vídeo Lyric Oficial)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 }
 
-function change_picture(direction){
-    if(direction < 0 && current_picture == 0){
-        current_picture = PICTURE[current_device].length - 1
+function change_picture(direction, key='') {
+    let img = document.querySelector('img')
+    if (key == '') {
+        key = current_device
+        console.log(key)
     }
-    else if(direction > 0 && current_picture == PICTURE[current_device].length - 1){
+    if (direction < 0 && current_picture == 0) {
+            current_picture = PICTURE[key].length - 1
+
+    }
+    else if (direction > 0 && current_picture == PICTURE[key].length - 1) {
         current_picture = 0
     }
-    else{
+    else {
         current_picture += direction
     }
-    let img = document.querySelector('img')
-    img.src = `static/imgs/${current_device}/${PICTURE[current_device][current_picture]}`
+    if (key == 'menbers'){
+        let name = document.querySelector('#name')
+        let description = document.querySelector('#description')
+        name.innerHTML = PICTURE[key][current_picture]['name']
+        description.innerHTML = PICTURE[key][current_picture]['description']
+        img.src = `static/imgs/${key}/${PICTURE[key][current_picture]['img']}`
+    }
+    else{
+        img.src = `static/imgs/${key}/${PICTURE[key][current_picture]}`
+
+    }
 }
+console.log(PICTURE)
